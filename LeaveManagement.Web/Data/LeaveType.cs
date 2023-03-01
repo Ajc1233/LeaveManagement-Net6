c2b38a655bbe520efec70ac8ada05ef10d016a0f
AJC <aleccampos10@yahoo.com>
2023-02-28 18:12:10 -0600

    Added database tables and migrations.

---
+++ LeaveManagement.Web/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagement.Web.Data.Migrations
+{
+    public class LeaveType : BaseEntity
+    {
+
+        public string Name { get; set; }
+        public int DefaultDays { get; set; }
+
+
+    }
+}
